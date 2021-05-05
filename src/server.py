181830bf19957b43fc44d2b9f9bb9403b18c1634
JopaXd <pavle_milanov@hotmail.com>
2021-05-05 17:07:37 +0200

    Made the server send/receieve the amount of data before sending the actual data.

--- src/server.py
+++ src/server.py
@@ -3,8 +3,10 @@ import threading
 import logging
 import json
 
-#TODO: Think about commands !disconnect and !users and if its actually a good idea to do it this way, and is there a better way.
 #TODO: Make the closing of the server much easier when running from cmd.
+
+#Thought about the commands system thing i did down there.
+#When one of tehse commands is executed just make sure that the client doesn't display anything. Simple as that.
 
 class Client:
 	def __init__(self, connection, username=None):
@@ -12,11 +14,13 @@ class Client:
 		self.username = username
 
 class Server:
-	def __init__(self, port=7802):
+	def __init__(self, port=7802, recv_data=2048):
 		self.port = port
 		self.ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.thread_count = 0
 		self.clients = []
+		self.recv_data = recv_data
+		self.encoding_format = "utf-8"
 
 	@property
 	def ip_address(self):
@@ -37,31 +41,45 @@ class Server:
 		while True:
 			if not user_data_acquired:
 				#User data contains just the username, so its just one string.
-				user_data = client.connection.recv(2048)
-				client.username = user_data.decode("utf-8")
+				user_data = client.connection.recv(self.recv_data)
+				client.username = user_data.decode(self.encoding_format)
 				user_data_acquired = True
 			else:
 				#Got the data about the user needed now wait for messages.
-				data = client.connection.recv(2048)
+				#Get the amount of data that is being expected.
+				amt_data = int(client.connection.recv(self.recv_data).decode(self.encoding_format))
+				logging.info(f"Recieved: {amt_data} bytes of data!")
+				#Get the actual data.
+				data = client.connection.recv(amt_data)
 				if not data:
 					pass
-				if (text := data.decode("utf-8")) == "!disconnect":
+				if (text := data.decode(self.encoding_format)) == "!disconnect":
 					client.connection.close()
+					self.clients.remove(client)
 					logging.info(f"Client disconnected!")
 					break
 				elif text == "!users":
 					#return users
 					pass
 				else:
-					message = str.encode(f"{client.username}: {text}")
+					message = f"{client.username}: {text}".encode(self.encoding_format)
+					logging.info(text)
 					self.broadcast_message(message)
 
 	def broadcast_message(self, msg):
 		for client in self.clients:
+			#Send the amount of data being sent
+			msg_size = len(msg)
+			#First send the message size
+			client.connection.sendall(str(msg_size).encode(self.encoding_format))
+			#Then send the actual data.
 			client.connection.sendall(msg)
 
 	def start_server(self):
 		self.ss.bind((self.ip_address, self.port))
+		logging.info("Server online!")
+		logging.info(f"Address: {self.ip_address}")
+		logging.info(f"Port: {self.port}")
 		while True:
 			self.ss.listen()
 			Client, address = self.ss.accept()
